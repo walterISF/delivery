498714f3676403e4d355549a8736a4c97e0559d6
walterISF <wisf_94@hotmail.com>
2016-09-19 21:35:28 -0300

    subindo projeto delivery

---
+++ js/funcoes.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$(".button-collapse").sideNav();
+$('.modal-trigger').leanModal();
+$('#push,secton').pushpin({ top:$('#push').height() });
+
+
+
+/*Obtendo dados do formulario com a funcao submit do proprio*/
+$('#formulario').submit(function(){
+	var dados = $('#formulario').serialize();
+	alert(dados);
+	/*$.ajax({  
+	          type: "POST",  
+	          url: ".php",  
+	          data: dados,  
+	          success: function( data )  
+	          { 
+				     
+	          }  
+     	  });*/  
+
+});
+
+$('#rbTelefone').click(function(){
+	$('#icnEscolha').html('phone');
+	$('#lblEscolha').html('Telefone');
+	$('#ipEscolha').attr("type", "tel");
+});
+
+$('#rbEmail').click(function(){
+	$('#icnEscolha').html('mail_outline');
+	$('#lblEscolha').html('Email');
+	$('#ipEscolha').attr("type", "email");
+});
+
+ });
