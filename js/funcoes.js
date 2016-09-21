6c4f5cfca5e47d2ec216d839c10d815934c9f59a
walterISF <wisf_94@hotmail.com>
2016-09-21 15:17:58 -0300

    adicionando footer

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$(".button-collapse").sideNav();
+$(".button-collapse").sideNav();
+$('select').material_select();
 $('.modal-trigger').leanModal();
 $('#push,secton').pushpin({ top:$('#push').height() });
 
